ffcc315971f898d3a3ecf5b309ac173da20c2166
raquelpinheiro <raqueljpinheiro@gmail.com>
2019-03-20 08:43:51 -0300

    Add files via upload

---
+++ OpimaCPJ.UI/Migrations/20190319183341_Migration2.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OpimaCPJ.Web.Migrations
+{
+    public partial class Migration2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Endereco",
+                columns: table => new
+                {
+                    Codigo = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Rua = table.Column<string>(nullable: true),
+                    Numero = table.Column<string>(nullable: true),
+                    Cep = table.Column<string>(nullable: true),
+                    Complemento = table.Column<string>(nullable: true),
+                    TelefoneResidencial = table.Column<string>(nullable: true),
+                    TelefoneCelular = table.Column<string>(nullable: true),
+                    TipoEndereco = table.Column<string>(nullable: true),
+                    CodigoBairro = table.Column<int>(nullable: true),
+                    CodigoPessoa = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Endereco", x => x.Codigo);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Endereco");
+        }
+    }
+}
